fad5860d309045b3461c7b65136e05d7e9cf12a8
vantam8300 <vantam8300@gmail.com>
2022-11-18 20:39:38 -0800

    finish generating team profile

--- src/app.js
+++ src/app.js
@@ -63,7 +63,7 @@ const engineerQuestions = [
 
     {
         type: "input",
-        name: "username",
+        name: "github",
         message: "What is engineer's Github username?"
     },
 
@@ -128,37 +128,147 @@ class App {
         const manager = await inquirer.prompt(managerQuestions)
         this.employeeList.push(new Manager(manager.name, manager.id, manager.email, manager.officeNumber))
         let nextAction = manager.action;
-    
+
         // user want to add engineer or intern
         while (nextAction !== "Done") {
-    
+
             if (nextAction === "Adding Engineer") {
-    
+
                 // prompt user for engineer information
                 const engineer = await inquirer.prompt(engineerQuestions)
-                this.employeeList.push(new Engineer(engineer.name, engineer.id, engineer.email, engineer.username));
+                this.employeeList.push(new Engineer(engineer.name, engineer.id, engineer.email, engineer.github));
                 nextAction = engineer.action;
-    
+
             } else {
-    
+
                 // prompt user for intern information
                 const intern = await inquirer.prompt(internQuestions)
                 this.employeeList.push(new Intern(intern.name, intern.id, intern.email, intern.school));
                 nextAction = intern.action;
             }
         }
-    
+
         // user want to finish building the team information
         console.log("Your team profile has been generated!");
 
         this.generateFile();
     }
 
-    generateFile() {  
+
+    generateFile() {
+
+        let content = `<!DOCTYPE html>
+        <html>
+        <head>
+            <meta charset='utf-8'>
+            <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+            <title>Team Profile</title>
+            <meta name='viewport' content='width=device-width, initial-scale=1'>
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+        </head>
         
+        <body>
+        
+            <header class="bg-danger h-25 text-center">
+                <h1 class="text-light">My Team</h1>
+            </header>
+        
+            <main class="h-75">
+                <section class="d-flex flex-wrap justify-content-around p-4">`
+
+            +
+            this.generateCard(this.employeeList)
+            +
+
+            `</section>
+            </main>
+        </body>
+        </html>`
+
+        fs.writeFileSync("./dist/teamProfile.html", content);
+    }
+
+    generateCard(employeeList) {
+        let result = ``;
+        employeeList.forEach(employee => {
+            console.log("inside generate Card",employee)
+
+            if (employee instanceof Manager) {
+                result += `
+                <div class="card shadow-lg m-4" style="width: 18rem;">
+                    <div class="bg-primary text-light pl-4 ">
+                        <h2>${employee.name}</h2>
+                        <h3>Manager</h3>
+                    </div>
+                    <div class="card-body bg-light">
+                        <table class="table bg-white table-bordered">
+                            <tbody>
+                                <tr>
+                                    <td>ID: ${employee.id}</td>
+                                </tr>
+                                <tr>
+                                    <td>Email: <a href="${employee.email}">${employee.email}</a></td>
+                                </tr>
+                                <tr>
+                                    <td>Office number: ${employee.officeNumber}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>`;
+            } else if (employee instanceof Engineer) {
+                result += `
+                <div class="card shadow-lg m-4" style="width: 18rem;">
+                    <div class="bg-primary text-light pl-4 ">
+                        <h2>${employee.name}</h2>
+                        <h3>Engineer</h3>
+                    </div>
+                    <div class="card-body bg-light">
+                        <table class="table bg-white table-bordered">
+                            <tbody>
+                                <tr>
+                                    <td>ID: ${employee.id}</td>
+                                </tr>
+                                <tr>
+                                    <td>Email: <a href="${employee.email}">${employee.email}</a></td>
+                                </tr>
+                                <tr>
+                                    <td>Github: <a href="https://github.com/${employee.github}">${employee.github}</a></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>`;
+            } else {
+                result += `
+                <div class="card shadow-lg m-4" style="width: 18rem;">
+                    <div class="bg-primary text-light pl-4 ">
+                        <h2>${employee.name}</h2>
+                        <h3>Intern</h3>
+                    </div>
+                    <div class="card-body bg-light">
+                        <table class="table bg-white table-bordered">
+                            <tbody>
+                                <tr>
+                                    <td>ID: ${employee.id}</td>
+                                </tr>
+                                <tr>
+                                    <td>Email: <a href="${employee.email}">${employee.email}</a></td>
+                                </tr>
+                                <tr>
+                                    <td>School: ${employee.school}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>`;
+            }
+        });
+        return result;
     }
 
 }
+
 
 
 module.exports = App;
